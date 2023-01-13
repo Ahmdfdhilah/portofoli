432ab1f0fab80f1c4e08c59436fb51d97d8a1634
Ahmad_Fadillah <ahmad.fadilah0210@gmail.com>
2023-01-13 21:31:19 +0700

    changing new profile photo and adding new professional skills

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -6,16 +6,35 @@ import {
   DiReact,
   DiNodejs,
   DiPython,
+  DiMysql,
   DiGit,
+  DiCss3,
+  DiSass,
+
 } from "react-icons/di";
 import {
-  SiVisualstudiocode
+  SiVisualstudiocode,
+  SiNextdotjs,
+  SiVuedotjs,
+  SiTailwindcss
 } from "react-icons/si";
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <CgCPlusPlus />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVuedotjs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTailwindcss />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiCss3 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiSass />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
@@ -31,6 +50,12 @@ function Techstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMysql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNextdotjs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode/>
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/profile-img.webp";
+import myImg from "../../Assets/profile-img3.png";
 import {
   AiFillGithub,
   AiOutlineTwitter,
